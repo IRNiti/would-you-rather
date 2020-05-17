c7ebb25bea3e13dca24a71d2db50ccb80d0cbec2
IRNiti <ioana.niti@mail.mcgill.ca>
2020-05-16 21:53:54 -0700

    feat: leaderboard style except score detail

--- src/components/LeaderboardUser.js
+++ src/components/LeaderboardUser.js
@@ -4,12 +4,31 @@ export default function LeaderboardUser(props){
     const answers = Object.keys(props.user.answers).length
     const questions = props.user.questions.length
     return(
-        <div>
-            <p>{props.user.name}</p>
-            <img src={props.user.avatarURL} alt={`Avatar of ${props.user.name}`} className='avatar'/>
-            <p>{`Answered questions: ${answers}`}</p>
-            <p>{`Created questions: ${questions}`}</p>
-            <p>{`Score: ${answers + questions}`}</p>
+        <div className='card card-single'>
+            <div className='question-detail'>
+                <img src={props.user.avatarURL} alt={`Avatar of ${props.user.name}`} className='avatar'/>
+                <div className='vl'/>
+                <div className='question'>
+                    <h3 className='card-title'>{props.user.name}</h3>
+                    <div className='leaderboard-results'>
+                        <div className='leaderboard-line'>
+                            <p className='left'>Answered questions</p>
+                            <p className='right'>{answers}</p>
+                        </div>
+                        <div className='hb'/>
+                        <p>{`Created questions: ${questions}`}</p>
+                    </div>
+                </div>
+                <div className='vl'/>
+                <div className='card card-score'>
+                    <div className="card-header center">
+                        <h6>Score</h6>
+                    </div>
+                    <h2>
+                        <span className="badge badge-pill badge-primary badge-score">{answers + questions}</span>
+                    </h2>
+                </div>
+            </div>
         </div>
     )
 }
