98be8a9ec2cf7a066a79285ee6ba59845c47ddda
IRNiti <ioana.niti@mail.mcgill.ca>
2020-05-16 18:59:06 -0700

    feat: styling element alignment for question cards in question list

--- src/components/Question.js
+++ src/components/Question.js
@@ -6,12 +6,22 @@ class Question extends Component{
     render() {
         const {question, users, id} = this.props
         return(
-            <Link to={`/question/${id}`} style={{ color: 'inherit', textDecoration: 'inherit'}}>
-                <p>{users[question.author].name} asks: </p>
-                <img src={users[question.author].avatarURL} alt={`Avatar of ${users[question.author].name}`} className='avatar'/>
-                <p>Would you rather: </p>
-                <p>...{question.optionOne.text}...</p>
-            </Link>
+            <div className='card card-question'>
+
+                    <div className="card-header">
+                        <p>{users[question.author].name} asks: </p>
+                    </div>
+                    <div className='question-detail'>
+                        <img src={users[question.author].avatarURL} alt={`Avatar of ${users[question.author].name}`} className='avatar'/>
+                        <div className='question center'>
+                            <h5>Would you rather: </h5>
+                            <p>...{question.optionOne.text}...</p>
+                            <Link to={`/question/${id}`} className='link-question'>
+                                <button className='btn btn-primary'>View Poll</button>
+                            </Link>
+                        </div>
+                    </div>
+            </div>
         )
     }
 }
