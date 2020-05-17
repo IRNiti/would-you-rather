698d843fa58177a0784bf06512e39b6606f1a4ec
IRNiti <ioana.niti@mail.mcgill.ca>
2020-05-16 21:06:48 -0700

    feat: adding styling to new question page

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -36,31 +36,36 @@ class NewQuestion extends Component {
 
     render() {
         return(
-            <div>
-                <h2>Create New Question</h2>
-                <p>Complete the question</p>
-                <h3>Would you rather...</h3>
-                <form onSubmit={this.handleSubmit}>
-                    <div>
-                        <input
-                            type='text'
-                            name='optionOne'
-                            placeholder='Enter Option One Text Here...'
-                            value={this.state.optionOne}
-                            onChange={this.handleChange}
-                        />
-                        <p>OR</p>
-                        <input
-                            type='text'
-                            name='optionTwo'
-                            placeholder='Enter Option Two Text Here...'
-                            value={this.state.optionTwo}
-                            onChange={this.handleChange}
-                        />
-                    </div>
-                    <button type='submit'>Submit</button>
-                </form>
-
+            <div className='card card-single'>
+                <div className="card-header">
+                    <h2>Create New Question</h2>
+                </div>
+                <div className='question'>
+                    <p>Complete the question</p>
+                    <h4 className='question-new-title'>Would you rather...</h4>
+                    <form onSubmit={this.handleSubmit} className='center'>
+                        <div>
+                            <input
+                                type='text'
+                                name='optionOne'
+                                placeholder='Enter Option One Text Here...'
+                                value={this.state.optionOne}
+                                onChange={this.handleChange}
+                                className='question-input'
+                            />
+                            <p>OR</p>
+                            <input
+                                type='text'
+                                name='optionTwo'
+                                placeholder='Enter Option Two Text Here...'
+                                value={this.state.optionTwo}
+                                onChange={this.handleChange}
+                                className='question-input'
+                            />
+                        </div>
+                        <button type='submit' className='btn btn-primary'>Submit</button>
+                    </form>
+                </div>
             </div>
         )
     }
