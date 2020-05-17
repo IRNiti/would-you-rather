a4baa421ebdbe540352434da3ae23cf3d328f092
IRNiti <ioana.niti@mail.mcgill.ca>
2020-05-16 23:27:46 -0700

    feat: rounding vote result to zero decimals

--- src/components/QuestionResultOption.js
+++ src/components/QuestionResultOption.js
@@ -3,7 +3,7 @@ import ProgressBar from 'react-bootstrap/ProgressBar'
 
 export default function QuestionResultOption(props){
     const {question, answer, totalVotes, option} = props
-    const progress = question[option].votes.length/totalVotes*100
+    const progress = (question[option].votes.length/totalVotes*100).toFixed(0)
     return(
         <div className='card card-option'>
             <p>
