1ec2662c7d210cd709c93d5f9a17f8be83f835ba
IRNiti <ioana.niti@mail.mcgill.ca>
2020-05-18 22:01:17 -0700

    feat: implement 404 page when question id does not exist

---
+++ src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function NotFound(){
+    return(
+        <div>
+            <p>404 PAGE NOT FOUND</p>
+            <p>The question id you entered probably doesn't exist</p>
+        </div>
+    )
+}
--- src/components/QuestionCont.js
+++ src/components/QuestionCont.js
@@ -2,25 +2,29 @@ import React, { Component } from 'react'
 import { connect } from 'react-redux'
 import QuestionVote from './QuestionVote'
 import QuestionResult from './QuestionResult'
+import NotFound from './NotFound'
 
 class QuestionCont extends Component{
     render(){
-
+        console.log('props in question cont: ', this.props)
         return(
             <div>
-                {this.props.answered ?
+                {this.props.question ? (
+                this.props.answered ?
                     (<QuestionResult id={this.props.match.params.id}/>) :
-                    (<QuestionVote id={this.props.match.params.id}/>)}
+                    (<QuestionVote id={this.props.match.params.id}/>)) :
+                    (<NotFound />)}
             </div>
         )
     }
 }
 
-function mapStateToProps({authedUser, users},{match}){
+function mapStateToProps({authedUser, users, questions},{match}){
     const qid = match.params.id
 
     return {
-        answered: authedUser && users[authedUser].answers[qid]
+        answered: authedUser && users[authedUser].answers[qid],
+        question: questions[qid]
     }
 }
 
