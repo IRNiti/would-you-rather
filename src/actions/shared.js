01b7eb3f018a02746f6d663083aa057491467812
IRNiti <ioana.niti@mail.mcgill.ca>
2020-05-19 17:44:34 -0700

    refactor: removing unnecessary files and packages

--- src/actions/shared.js
+++ src/actions/shared.js
@@ -1,15 +1,12 @@
 import { _getUsers, _getQuestions } from '../utils/_DATA'
 import { receiveUsers } from './users'
 import { receiveQuestions } from './questions'
-import { showLoading, hideLoading } from 'react-redux-loading'
 
 export function handleInitialData() {
 	return (dispatch) => {
-		dispatch(showLoading())
 		return Promise.all([_getUsers(), _getQuestions()]).then(([users, questions]) => {
 			dispatch(receiveUsers(users))
 			dispatch(receiveQuestions(questions))
-			dispatch(hideLoading())
 		})
 	}
 }
