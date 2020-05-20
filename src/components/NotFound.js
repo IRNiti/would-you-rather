4c7428c55121d9cce5835edd5d44327aed298122
IRNiti <ioana.niti@mail.mcgill.ca>
2020-05-19 18:50:30 -0700

    docs: adding image source

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -1,6 +1,7 @@
 import React from 'react'
 
 // Presentational component displaying 404 Not Found page
+// image source: https://dribbble.com/shots/5336717-404-Page-Not-Found/attachments
 export default function NotFound(){
     return(
         <div className='center'>
